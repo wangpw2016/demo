9211a224c69b330694e00653d198fd89bb3648ec
pengwei.wang <wangpengwei@qiaofang.info>
2019-01-23 11:17:19 +0800

    Bug:20013; Subject:Fix business bug; Reviewer: wpw;

--- src/main/java/com/example/demo/IndexControl.java
+++ src/main/java/com/example/demo/IndexControl.java
@@ -4,12 +4,15 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Map;
+
 @Controller
 public class IndexControl   {
 
 @GetMapping("/")
-public String Index(){
+public String Index(Map<String ,String> map){
 
+    map.put("name","wangpw  hello111" );
 
     return "index";
 }
