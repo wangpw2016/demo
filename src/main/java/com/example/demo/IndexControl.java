3e40e13783bd67efad07fe45b997986298fe3432
pengwei.wang <wangpengwei@qiaofang.info>
2019-01-23 10:40:29 +0800

    Bug:20013; Subject:Fix business bug; Reviewer: super man;

---
+++ src/main/java/com/example/demo/IndexControl.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexControl   {
+
+@GetMapping("/")
+public String Index(){
+
+
+    return "index";
+}
+
+}
